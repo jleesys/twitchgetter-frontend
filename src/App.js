563a2254e851d6ad317f03f06ca4e2e0b17b8dbd
jleesys949 <jlee.post24@gmail.com>
2023-01-15 21:10:39 -0800

    Project creation

--- src/App.js
+++ src/App.js
@@ -1,23 +1,23 @@
-import logo from './logo.svg';
-import './App.css';
+import { useState } from 'react';
 
 function App() {
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
+      hmm?
+      <div>
+        <a href="https://id.twitch.tv/oauth2/authorize?response_type=code&client_id=2neu10boac0rm819dtozub32wj1yaf&redirect_uri=http://localhost:3001/&scope=user%3Aread%3Afollows&state=c3ab8aa609ea11e793ae92361f002671/">
+          Connect with Twitch
+          Grants An Authorization Code
         </a>
-      </header>
+        <div>
+          <a href="https://id.twitch.tv/oauth2/token?client_id=2neu10boac0rm819dtozub32wj1yaf&client_secret=f1imepjq84xmrmv2cfi30340e6riu3&code=jd538t5yy46aux5n1hms3zut6yxj6k&grant_type=authorization_code&redirect_uri=http://localhost:3001">
+            Get Authorization Token
+          </a>
+        </div>
+      </div>
+
+
     </div>
   );
 }
